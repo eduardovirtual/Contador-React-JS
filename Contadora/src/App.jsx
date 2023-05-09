cfeecf0bdf5f12a500c4dcffc63c7643428c5f47
Eduardo dos Santos da Costa <127545505+eduardovirtual@users.noreply.github.com>
2023-05-09 03:20:26 -0300

    Add files via upload

---
+++ Contadora/src/App.jsx
@@ -0,0 +1,17 @@
+import {useState} from 'react'
+
+function App() {
+  const [value, setValue] = useState(0)
+
+  return (
+    <div>
+      <h1>Contador Simples em React</h1>
+      <h2>{value}</h2>
+      <button className="botao" onClick={() => setValue(value + 1)}>Aumentar</button>
+      <button className="botao" onClick={() => setValue(value - 1)}>Diminuir</button>
+      <button className="botao" onClick={() => setValue(0)}>Resetar</button>
+    </div>
+  )
+}
+
+export default App
